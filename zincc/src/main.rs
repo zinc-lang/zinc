1e32416682075fa50e16cb629b9e1dd767f7e57a
Ketan Reynolds <ketan.cs.reynolds@gmail.com>
2022-06-17 23:53:59 +0200

    Use clap for argument parsing
    - Allow specifying what to verbosely print
    - Opt-in to print times

--- zincc/src/main.rs
+++ zincc/src/main.rs
@@ -12,8 +12,10 @@ pub mod util;
 fn main() {
     // zir_test::zir_test();
 
-    let args = std::env::args().collect::<Vec<_>>();
-    let source_path = &args[1];
+    let options = get_options();
+
+    assert_eq!(options.files.len(), 1);
+    let source_path = &options.files[0];
 
     let source = util::read_file_to_string(source_path)
         .map(|s| if !s.ends_with('\n') { s + "\n" } else { s })
@@ -28,19 +30,21 @@ fn main() {
     let lex_res = parse::lex(&source);
     let duration_lex = stopwatch.read();
 
-    // // @Verbose: Printing tokens
-    // lex_res.debug_zip().for_each(|(tk, range, _)| {
-    //     debug::print_token(&source, tk, range);
-    // });
-    // println!();
+    if options.verbose_tokens {
+        lex_res.debug_zip().for_each(|(tk, range, _)| {
+            debug::print_token(&source, tk, range);
+        });
+        println!();
+    }
 
     stopwatch.reset();
     let parse_res = parse::parse(&lex_res.tokens);
     let duration_parse = stopwatch.read();
 
-    // @Verbose: Printing cst
-    debug::print_cst(&source, &parse_res.cst, &lex_res.tokens, &lex_res.spans, 0);
-    println!();
+    if options.verbose_cst {
+        debug::print_cst(&source, &parse_res.cst, &lex_res.tokens, &lex_res.spans, 0);
+        println!();
+    }
 
     if !parse_res.errors.is_empty() {
         for error in parse_res.errors {
@@ -68,17 +72,78 @@ fn main() {
     let ast = ast::gen::root(&parse_res.cst);
     let duration_astgen = stopwatch.read();
 
-    // @Verbose: printing ast
-    dbg!(&ast);
-    println!();
+    if options.verbose_ast {
+        dbg!(&ast);
+        println!();
+    }
 
-    // @Verbose: printing times
-    println!("times:");
-    println!("  lexing:\t{:?}", duration_lex);
-    println!("  parsing:\t{:?}", duration_parse);
-    println!("  astgen:\t{:?}", duration_astgen);
-    println!(
-        "  total:\t{:?}",
-        duration_lex + duration_parse + duration_astgen
-    );
+    if options.print_times {
+        println!("times:");
+        println!("  lexing:\t{:?}", duration_lex);
+        println!("  parsing:\t{:?}", duration_parse);
+        println!("  astgen:\t{:?}", duration_astgen);
+        println!(
+            "  total:\t{:?}",
+            duration_lex + duration_parse + duration_astgen
+        );
+    }
+}
+
+#[derive(Debug)]
+pub struct Options {
+    files: Vec<String>,
+
+    verbose_tokens: bool,
+    verbose_cst: bool,
+    verbose_ast: bool,
+
+    print_times: bool,
+}
+
+fn get_options() -> Options {
+    use clap::builder::PossibleValuesParser;
+    use clap::Arg;
+
+    let matches = clap::Command::new("zincc")
+        .arg(Arg::new("FILES").required(true).multiple_values(true))
+        .arg(
+            Arg::new("verbose")
+                .long("verbose")
+                .short('v')
+                .value_parser(PossibleValuesParser::new(&["tokens", "cst", "ast"]))
+                .takes_value(true)
+                .action(clap::ArgAction::Set),
+        )
+        .arg(
+            Arg::new("print_times")
+                .long("print_times")
+                .short('T')
+                .help("Print how long processes took"),
+        )
+        .get_matches();
+
+    let files = matches
+        .get_many::<String>("FILES")
+        .unwrap()
+        .cloned()
+        .collect();
+
+    let verbose = matches
+        .get_many::<String>("verbose")
+        .map(|s| s.cloned().collect::<Vec<String>>())
+        .unwrap_or(vec![]);
+
+    let verbose_tokens = verbose.contains(&"tokens".to_string());
+    let verbose_cst = verbose.contains(&"cst".to_string());
+    let verbose_ast = verbose.contains(&"ast".to_string());
+
+    let print_times = matches.contains_id("print_times");
+
+    Options {
+        files,
+        verbose_tokens,
+        verbose_cst,
+        verbose_ast,
+        print_times,
+    }
 }
