53e195ddec9bb053a65e3fc751a9ad261fbc7f3c
Ketan Reynolds <ketan.cs.reynolds@gmail.com>
2022-06-24 13:58:36 +0200

    Optimize the lexer
    I managed to get it down from 70-100ms to 80-120µs when lexing c.zinc.
    That's close to a 100x speed improvement, nice
    
    Now the slowest part of the pipeline at the moment is the astgen. Even
    with an optimized build. There's definitely room for improvement there.

--- zincc/src/parse/lexer.rs
+++ zincc/src/parse/lexer.rs
@@ -32,7 +32,7 @@ impl LexResult {
 }
 
 struct Lexer<'s> {
-    source: &'s str,
+    ascii: &'s [u8],
 
     ws_len: u16,
     tokens: Vec<TK>,
@@ -45,7 +45,7 @@ struct Lexer<'s> {
 impl<'s> Lexer<'s> {
     fn new(source: &'s str) -> Self {
         Self {
-            source,
+            ascii: source.as_bytes(),
             ws_len: 0,
             tokens: vec![],
             spans: vec![],
@@ -61,36 +61,36 @@ impl Lexer<'_> {
             let ch = self.advance();
 
             match ch {
-                ' ' | '\n' | '\t' | '\r' => {
+                b' ' | b'\n' | b'\t' | b'\r' => {
                     self.inc_ws();
-                    while matches!(self.peek(), ' ' | '\n' | '\t' | '\r') {
-                        assert_ne!(self.advance(), '\0');
+                    while matches!(self.peek(), b' ' | b'\n' | b'\t' | b'\r') {
+                        assert_ne!(self.advance(), b'\0');
                         self.inc_ws();
                     }
                 }
 
-                '(' => self.tok(TK::brkt_paren_open),
-                ')' => self.tok(TK::brkt_paren_close),
-                '{' => self.tok(TK::brkt_brace_open),
-                '}' => self.tok(TK::brkt_brace_close),
-                '[' => self.tok(TK::brkt_square_open),
-                ']' => self.tok(TK::brkt_square_close),
+                b'(' => self.tok(TK::brkt_paren_open),
+                b')' => self.tok(TK::brkt_paren_close),
+                b'{' => self.tok(TK::brkt_brace_open),
+                b'}' => self.tok(TK::brkt_brace_close),
+                b'[' => self.tok(TK::brkt_square_open),
+                b']' => self.tok(TK::brkt_square_close),
 
-                ':' => self.tok_if_match(':', TK::punct_dblColon, TK::punct_colon),
-                ';' => self.tok(TK::punct_semiColon),
-                ',' => self.tok(TK::punct_comma),
+                b':' => self.tok_if_match(b':', TK::punct_dblColon, TK::punct_colon),
+                b';' => self.tok(TK::punct_semiColon),
+                b',' => self.tok(TK::punct_comma),
 
-                '=' => self.tok_if_match('>', TK::punct_fat_arrow, TK::punct_eq),
-                '+' => self.tok(TK::punct_plus),
-                '-' => self.tok(TK::punct_minus),
-                '*' => self.tok(TK::punct_star),
+                b'=' => self.tok_if_match(b'>', TK::punct_fat_arrow, TK::punct_eq),
+                b'+' => self.tok(TK::punct_plus),
+                b'-' => self.tok(TK::punct_minus),
+                b'*' => self.tok(TK::punct_star),
 
-                '/' => {
-                    if self.match_next('/') {
+                b'/' => {
+                    if self.match_next(b'/') {
                         self.inc_ws();
                         self.inc_ws(); // '//'
-                        while !matches!(self.peek(), '\n' | '\0') {
-                            assert_ne!(self.advance(), '\0');
+                        while !matches!(self.peek(), b'\n' | b'\0') {
+                            assert_ne!(self.advance(), b'\0');
                             self.inc_ws();
                         }
                     } else {
@@ -98,22 +98,22 @@ impl Lexer<'_> {
                     }
                 }
 
-                '"' => self.lex_string(),
+                b'"' => self.lex_string(),
 
-                '\0' => break 'lex_loop,
+                b'\0' => break 'lex_loop,
 
                 _ => {
-                    if ch == '0' {
+                    if ch == b'0' {
                         match self.peek() {
-                            'x' => {
+                            b'x' => {
                                 self.lex_number_generic(TK::int_hex, is_char::number_hex);
                                 continue 'lex_loop;
                             }
-                            'b' => {
+                            b'b' => {
                                 self.lex_number_generic(TK::int_bin, is_char::number_binary);
                                 continue 'lex_loop;
                             }
-                            'o' => {
+                            b'o' => {
                                 self.lex_number_generic(TK::int_oct, is_char::number_octal);
                                 continue 'lex_loop;
                             }
@@ -124,7 +124,13 @@ impl Lexer<'_> {
                     match ch {
                         _ if is_char::number_start(ch) => self.lex_number(),
                         _ if is_char::ident_start(ch) => self.lex_ident(),
-                        _ => todo!("report error; unknown char"),
+                        _ => todo!(
+                            "report error; unknown char {:?}",
+                            crate::parse::FileLocation::from_offset(
+                                self.span.end,
+                                &String::from_utf8(self.ascii.to_vec()).unwrap(),
+                            )
+                        ),
                     }
                 }
             }
@@ -145,50 +151,33 @@ impl Lexer<'_> {
         self.span.start = self.span.end;
     }
 
-    // fn advance(&mut self) -> Option<char> {
-    //     let c = self.source.chars().nth(self.span.end as usize);
-    //     self.span.end += 1;
-    //     c
-    // }
-    //
-    // fn advance_assume(&mut self) -> char {
-    //     self.advance().unwrap()
-    // }
     #[must_use]
-    fn advance(&mut self) -> char {
-        let c = self
-            .source
-            .chars()
-            .nth(self.span.end as usize)
-            .unwrap_or('\0');
+    fn advance(&mut self) -> u8 {
         self.span.end += 1;
-        c
+        self.ascii[self.span.end - 1]
     }
 
-    fn peek_raw(&self, n: usize) -> char {
-        self.source
-            .chars()
-            .nth(self.span.end as usize + n)
-            .unwrap_or('\0')
+    fn peek_raw(&self, n: usize) -> u8 {
+        self.ascii[self.span.end + n]
     }
 
     #[inline]
-    fn peek(&self) -> char {
+    fn peek(&self) -> u8 {
         self.peek_raw(0)
     }
 
     #[inline]
-    fn peek_next(&self) -> char {
+    fn peek_next(&self) -> u8 {
         self.peek_raw(1)
     }
 
     #[inline]
-    fn at(&self, ch: char) -> bool {
+    fn at(&self, ch: u8) -> bool {
         self.peek() == ch
     }
 
     #[inline]
-    fn match_next(&mut self, expect: char) -> bool {
+    fn match_next(&mut self, expect: u8) -> bool {
         let cond = self.at(expect);
         if cond {
             let _ = self.advance();
@@ -196,7 +185,7 @@ impl Lexer<'_> {
         cond
     }
 
-    fn tok_if_match(&mut self, expect: char, r#if: TK, r#else: TK) {
+    fn tok_if_match(&mut self, expect: u8, r#if: TK, r#else: TK) {
         let m = self.match_next(expect);
         if m {
             self.tok(r#if);
@@ -212,14 +201,14 @@ impl Lexer<'_> {
     }
 
     #[inline]
-    fn lex_number_generic(&mut self, kind: TK, condition: fn(char) -> bool) {
-        assert_ne!(self.advance(), '\0');
+    fn lex_number_generic(&mut self, kind: TK, condition: fn(u8) -> bool) {
+        assert_ne!(self.advance(), b'\0');
         self.lex_while_condition(condition);
         self.tok(kind);
     }
 
     #[inline]
-    fn lex_while_condition(&mut self, condition: fn(char) -> bool) {
+    fn lex_while_condition(&mut self, condition: fn(u8) -> bool) {
         while condition(self.peek()) {
             let _ = self.advance();
         }
@@ -229,30 +218,30 @@ impl Lexer<'_> {
         self.tok(TK::string_open);
 
         let mut p = self.peek();
-        while p != '\"' && p != '\0' {
-            if p == '\\' {
+        while p != b'\"' && p != b'\0' {
+            if p == b'\\' {
                 if self.span.start != self.span.end {
                     self.tok(TK::string_literal);
                 }
 
-                assert_ne!(self.advance(), '\0');
+                assert_ne!(self.advance(), b'\0');
                 self.lex_escape();
             } else {
-                assert_ne!(self.advance(), '\0');
+                assert_ne!(self.advance(), b'\0');
             }
             p = self.peek();
         }
         if self.span.start != self.span.end {
             self.tok(TK::string_literal);
         }
-        assert_eq!(self.advance(), '\"');
+        assert_eq!(self.advance(), b'\"');
 
         self.tok(TK::string_close);
     }
 
     fn lex_escape(&mut self) {
         let kind = match self.advance() {
-            'x' => {
+            b'x' => {
                 let mut lex_char = || {
                     if !is_char::number_hex(self.advance()) {
                         todo!("report error; non hex char as ascii escape")
@@ -263,8 +252,8 @@ impl Lexer<'_> {
 
                 TK::esc_asciicode
             }
-            'u' => todo!("unicode escape lexing"),
-            '\0' => todo!("report error; eof when expecting escape"),
+            b'u' => todo!("unicode escape lexing"),
+            b'\0' => todo!("report error; eof when expecting escape"),
             _ => TK::esc_char,
         };
         self.tok(kind);
@@ -275,14 +264,14 @@ impl Lexer<'_> {
 
         self.lex_while_condition(is_char::number_mid);
 
-        if self.at('.') && is_char::number_mid(self.peek_next()) {
+        if self.at(b'.') && is_char::number_mid(self.peek_next()) {
             kind = TK::float;
-            assert_ne!(self.advance(), '\0');
+            assert_ne!(self.advance(), b'\0');
 
             self.lex_while_condition(is_char::number_mid);
-            if self.match_next('E') {
+            if self.match_next(b'E') {
                 match self.peek() {
-                    '+' | '-' => assert_ne!(self.advance(), '\0'),
+                    b'+' | b'-' => assert_ne!(self.advance(), b'\0'),
                     _ => todo!("report error; malformed float literal"),
                 }
                 self.lex_while_condition(is_char::number_mid);
@@ -295,41 +284,40 @@ impl Lexer<'_> {
     fn lex_ident(&mut self) {
         self.lex_while_condition(is_char::ident_mid);
 
-        let slice = self.source.chars().collect::<Vec<_>>();
-        let slice = &slice[self.span.start as usize..self.span.end as usize];
+        let slice = &self.ascii[self.span.clone()];
 
         let kind = match slice[0] {
             // const
-            'c' => self.check_kw(slice, 1, "onst", TK::kw_const),
+            b'c' => self.check_kw(slice, 1, "onst".as_bytes(), TK::kw_const),
 
-            // fn
             // false
-            'f' => match slice[1] {
-                'a' => self.check_kw(slice, 2, "lse", TK::kw_false),
-                'n' => self.check_kw(slice, 2, "", TK::kw_fn), // @Speed
+            // fn
+            b'f' => match slice[1] {
+                b'a' => self.check_kw(slice, 2, "lse".as_bytes(), TK::kw_false),
+                b'n' => self.check_kw(slice, 2, &[], TK::kw_fn), // @Speed
                 _ => TK::ident,
             },
 
             // let
-            'l' => self.check_kw(slice, 1, "et", TK::kw_let),
+            b'l' => self.check_kw(slice, 1, "et".as_bytes(), TK::kw_let),
 
             // mut
-            'm' => self.check_kw(slice, 1, "ut", TK::kw_mut),
+            b'm' => self.check_kw(slice, 1, "ut".as_bytes(), TK::kw_mut),
 
             // return
-            'r' => self.check_kw(slice, 1, "eturn", TK::kw_return),
+            b'r' => self.check_kw(slice, 1, "eturn".as_bytes(), TK::kw_return),
 
             // true
-            't' => self.check_kw(slice, 1, "rue", TK::kw_true),
+            b't' => self.check_kw(slice, 1, "rue".as_bytes(), TK::kw_true),
 
             _ => TK::ident,
         };
         self.tok(kind);
     }
 
-    fn check_kw(&self, slice: &[char], start: usize, rest: &str, kind: TK) -> TK {
+    fn check_kw(&self, slice: &[u8], start: usize, rest: &[u8], kind: TK) -> TK {
         let checking_slice = &slice[start..];
-        if checking_slice.iter().collect::<String>() == rest {
+        if checking_slice == rest {
             kind
         } else {
             TK::ident
@@ -339,37 +327,37 @@ impl Lexer<'_> {
 
 mod is_char {
     #[inline(always)]
-    pub fn ident_start(ch: char) -> bool {
-        ch.is_alphabetic() || ch == '_'
+    pub fn ident_start(ch: u8) -> bool {
+        matches!(ch, b'a'..=b'z' | b'A'..=b'Z' | b'_')
     }
 
     #[inline(always)]
-    pub fn ident_mid(ch: char) -> bool {
-        ch.is_alphanumeric() || ch == '_'
+    pub fn ident_mid(ch: u8) -> bool {
+        ident_start(ch) || number_start(ch)
     }
 
     #[inline(always)]
-    pub fn number_start(ch: char) -> bool {
-        ch.is_ascii_digit()
+    pub fn number_start(ch: u8) -> bool {
+        matches!(ch, b'0'..=b'9')
     }
 
     #[inline(always)]
-    pub fn number_mid(ch: char) -> bool {
-        number_start(ch) || ch == '_'
+    pub fn number_mid(ch: u8) -> bool {
+        number_start(ch) || ch == b'_'
     }
 
     #[inline(always)]
-    pub fn number_hex(ch: char) -> bool {
-        ch.is_ascii_hexdigit() || ch == '_'
+    pub fn number_hex(ch: u8) -> bool {
+        matches!(ch, b'0'..=b'9' | b'A'..=b'F' | b'a'..=b'f' | b'_')
     }
 
     #[inline(always)]
-    pub fn number_binary(ch: char) -> bool {
-        matches!(ch, '0' | '1' | '_')
+    pub fn number_binary(ch: u8) -> bool {
+        matches!(ch, b'0' | b'1' | b'_')
     }
 
     #[inline(always)]
-    pub fn number_octal(ch: char) -> bool {
-        matches!(ch, '0'..='7' | '_')
+    pub fn number_octal(ch: u8) -> bool {
+        matches!(ch, b'0'..=b'7' | b'_')
     }
 }
