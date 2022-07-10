4d82f3b051754f84f9f4bf014fe822cd57df5744
tealsnow <ketan.cs.reynolds@gmail.com>
2022-07-10 16:47:50 +0200

    workflows: Try a different directory

--- llvm_c_2-sys/build.rs
+++ llvm_c_2-sys/build.rs
@@ -1,6 +1,15 @@
+use std::process::Command;
+
 extern crate bindgen;
 
 fn main() {
+    let llvm_libdir = Command::new("llvm-config")
+        .arg("--libdir")
+        .output()
+        .unwrap();
+    let llvm_libdir = String::from_utf8(llvm_libdir.stdout).unwrap();
+
+    println!("cargo:rustc-link-search={}", llvm_libdir);
     println!("cargo:rustc-link-search=../llvm_c_2/build");
     println!("cargo:rustc-link-lib=llvm_c_2");
     println!("cargo:rustc-link-lib=c++");
