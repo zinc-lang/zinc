937343e6179e696a4d1036e1f01eb500cddab818
Ketan Reynolds <ketan.cs.reynolds@gmail.com>
2022-07-12 23:03:15 +0200

    zincc typer: Get some good type checking done
    This is going along very nicely. So far it has been much easier then the
    name resolver, I think that is because I thought this would be harder
    and had put a lot more thought into it. In hindsight the name resolver
    is pretty simple, but brand new problems tend to feel much harder then
    they end up being.

--- zincc/src/typer.rs
+++ zincc/src/typer.rs
@@ -1,4 +1,5 @@
 use crate::{
+    ast,
     nameres::{self as nr, NameResolutionResult},
     util::index::{self, InterningIndexVec},
 };
@@ -16,39 +17,61 @@ index::define_idx! { pub struct TyId: u32 }
 #[derive(Debug, PartialEq, Eq)]
 pub enum Ty {
     Prim(TyPrim),
-    Func {
-        args: SmallVec<[TyId; 4]>,
-        ret: TyId,
-    },
+    Func(TyFuncId),
     Slice(TyId),
     Nullable(TyId),
 }
 
+impl Ty {
+    pub fn as_prim(&self) -> Option<&TyPrim> {
+        if let Self::Prim(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
+    pub fn as_func(&self) -> Option<&TyFuncId> {
+        if let Self::Func(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+}
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum TyPrim {
-    Sint(TyIntSize),
-    Uint(TyIntSize),
+    Integer {
+        signed: bool,
+        size: Option<NonZeroU8>, // None => pointer sized
+    },
     Bool,
     Void,
     Never,
 }
 
+#[derive(Debug, Default)]
+pub struct TypeMap {
+    // pub locals: HashMap<nr::LocalId, TyId>,
+    pub exprs: HashMap<nr::ExprId, TyId>,
+
+    pub tys: InterningIndexVec<TyId, Ty>,
+    pub func_tys: InterningIndexVec<TyFuncId, TyFunc>,
+}
+
+index::define_idx! { pub struct TyFuncId: u32 }
+
 #[derive(Debug, PartialEq, Eq)]
-pub enum TyIntSize {
-    PtrSized,
-    BitSize(NonZeroU8),
+pub struct TyFunc {
+    pub params: SmallVec<[TyId; 4]>,
+    pub ret: TyId,
 }
 
 #[derive(Debug)]
 struct Typer<'nr> {
     nr: &'nr NameResolutionResult,
     map: TypeMap,
-}
-
-#[derive(Debug, Default)]
-pub struct TypeMap {
-    pub exprs: HashMap<nr::ExprId, TyId>,
-    pub tys: InterningIndexVec<TyId, Ty>,
 }
 
 impl<'nr> Typer<'nr> {
@@ -62,34 +85,203 @@ impl<'nr> Typer<'nr> {
     fn resolve(&mut self) {
         self.nr
             .map
-            .exprs
-            .indices()
-            .iter()
+            .decls
+            .keys()
             .cloned()
-            .for_each(|expr_id| {
-                let id = self.resolve_expr(expr_id);
-                self.map.exprs.insert(expr_id, id);
+            .for_each(|decl_id| self.resolve_decl(decl_id));
+
+        // assert that all exprs have a type
+        self.nr.map.exprs.indices().iter().for_each(|id| {
+            assert!(
+                self.map.exprs.contains_key(id),
+                "expr id({:?}), does not have a type",
+                id
+            )
+        });
+    }
+
+    fn resolve_decl(&mut self, decl_id: nr::DeclId) {
+        let decl = &self.nr.map.decls[&decl_id];
+        match decl {
+            nr::DeclKind::Func(func) => {
+                let _ = self.nr_ty_func_id_to_ty_func_id(func.ty);
+
+                self.resolve_expr(func.body);
+            }
+        }
+    }
+
+    fn nr_uty_id_to_ty_id(&mut self, uty: nr::UTyId) -> TyId {
+        let uty = &self.nr.map.utys[uty];
+        let ty = match uty {
+            nr::UTy::Res(res) => match res {
+                nr::TyPathResolution::PrimTy(prim) => {
+                    let prim = match prim {
+                        nr::PrimTy::Integer { signed, size } => TyPrim::Integer {
+                            signed: *signed,
+                            size: *size,
+                        },
+                        nr::PrimTy::Void => TyPrim::Void,
+                        nr::PrimTy::Bool => TyPrim::Bool,
+                    };
+                    Ty::Prim(prim)
+                }
+            },
+            nr::UTy::Func(id) => Ty::Func(self.nr_ty_func_id_to_ty_func_id(*id)),
+            nr::UTy::Slice(id) => Ty::Slice(self.nr_uty_id_to_ty_id(self.nr.map.tys[*id].id)),
+            nr::UTy::Nullable(id) => Ty::Nullable(self.nr_uty_id_to_ty_id(self.nr.map.tys[*id].id)),
+        };
+        self.map.tys.get_or_intern(ty)
+    }
+
+    fn nr_ty_func_id_to_ty_func_id(&mut self, ty_func: nr::TyFuncId) -> TyFuncId {
+        let func = &self.nr.map.func_tys[ty_func];
+        let params = func
+            .params
+            .iter()
+            .map(|&id| self.nr_uty_id_to_ty_id(self.nr.map.tys[id].id))
+            .collect();
+        let ret = func
+            .ret
+            .map_or(self.map.tys.get_or_intern(Ty::Prim(TyPrim::Void)), |id| {
+                self.nr_uty_id_to_ty_id(self.nr.map.tys[id].id)
             });
+
+        let func = TyFunc { params, ret };
+        self.map.func_tys.get_or_intern(func)
+    }
+
+    fn resolve_stmt(&mut self, stmt_id: nr::StmtId) {
+        let stmt = &self.nr.map.stmts[stmt_id];
+        match &stmt.kind {
+            nr::StmtKind::Let(_) => todo!(),
+            nr::StmtKind::Expr(expr_id) => {
+                self.resolve_expr(*expr_id);
+            }
+            nr::StmtKind::Decl(decl_id) => {
+                self.resolve_decl(*decl_id);
+            }
+        }
     }
 
     fn resolve_expr(&mut self, expr_id: nr::ExprId) -> TyId {
-        let expr = self.nr.map.exprs.get(expr_id).unwrap();
+        let expr = &self.nr.map.exprs[expr_id];
         let ty = match &expr.kind {
-            nr::ExprKind::Res(_) => todo!(),
-            nr::ExprKind::Block(_) => {
-                // @TODO: If block contains a return it should be of type never
+            nr::ExprKind::Res(res) => match res {
+                nr::ExprPathResolution::Decl(id) => {
+                    let decl = &self.nr.map.decls[id];
+                    match decl {
+                        nr::DeclKind::Func(func) => {
+                            let ty = Ty::Func(self.nr_ty_func_id_to_ty_func_id(func.ty));
+                            let id = self.map.tys.get_or_intern(ty);
+                            self.map.exprs.insert(expr_id, id);
+                            return id;
+                        }
+                    }
+                }
+                nr::ExprPathResolution::Local(_) => todo!(),
+                nr::ExprPathResolution::Arg(_) => todo!(),
+            },
+            nr::ExprKind::Block(block) => {
+                self.nr.map.blocks[*block]
+                    .stmts
+                    .iter()
+                    .cloned()
+                    .for_each(|stmt_id| self.resolve_stmt(stmt_id));
+
                 Ty::Prim(TyPrim::Void)
             }
             nr::ExprKind::Literal(literal) => match literal {
                 nr::ExprLiteral::String(_) => todo!(),
-                nr::ExprLiteral::Integer(_) => Ty::Prim(TyPrim::Sint(TyIntSize::PtrSized)),
+                nr::ExprLiteral::Integer(_) => Ty::Prim(TyPrim::Integer {
+                    signed: true,
+                    size: None,
+                }),
                 nr::ExprLiteral::Float(_) => todo!(),
                 nr::ExprLiteral::Bool(_) => Ty::Prim(TyPrim::Bool),
             },
-            nr::ExprKind::Infix(_) => todo!(),
-            nr::ExprKind::Call(_) => todo!(),
-            nr::ExprKind::Return(_) => Ty::Prim(TyPrim::Never),
+            nr::ExprKind::Infix(infix) => {
+                let lhs = self.resolve_expr(infix.lhs);
+                let rhs = self.resolve_expr(infix.rhs);
+
+                match infix.op {
+                    ast::ExprInfixOp::Equal => {
+                        if lhs != rhs {
+                            todo!("err")
+                        } else {
+                            return lhs;
+                        }
+                    }
+                    ast::ExprInfixOp::Add
+                    | ast::ExprInfixOp::Sub
+                    | ast::ExprInfixOp::Mul
+                    | ast::ExprInfixOp::Div => {
+                        let id = self.check_arithmetic_infix_op(lhs, rhs);
+                        self.map.exprs.insert(expr_id, id);
+                        return id;
+                    }
+                }
+            }
+            nr::ExprKind::Call(call) => {
+                let callee_ty = self.resolve_expr(call.callee);
+                let ty = &self.map.tys[callee_ty];
+
+                if let Some(id) = ty.as_func() {
+                    let func = &self.map.func_tys[*id];
+                    // @TODO: check args
+
+                    self.map.exprs.insert(expr_id, func.ret);
+                    return func.ret;
+                } else {
+                    todo!("err")
+                }
+            }
+            nr::ExprKind::Return(expr_id) => {
+                // @TODO: Check that type matches with function return type
+                let _ = expr_id.map(|id| self.resolve_expr(id));
+                Ty::Prim(TyPrim::Void)
+            }
         };
-        self.map.tys.get_or_intern(ty)
+        let id = self.map.tys.get_or_intern(ty);
+        self.map.exprs.insert(expr_id, id);
+        id
+    }
+
+    fn check_arithmetic_infix_op(&mut self, lhs: TyId, rhs: TyId) -> TyId {
+        let lhs_ty = &self.map.tys[lhs];
+        match lhs_ty {
+            Ty::Prim(prim) => match prim {
+                TyPrim::Integer { signed, size } => {
+                    let rhs_ty = &self.map.tys[rhs];
+                    if let Some(prim) = rhs_ty.as_prim() {
+                        match prim.as_integer() {
+                            Some((rhs_signed, rhs_size))
+                                if signed == rhs_signed && size == rhs_size =>
+                            {
+                                lhs
+                            }
+                            _ => todo!("err"),
+                        }
+                    } else {
+                        todo!("err")
+                    }
+                }
+                TyPrim::Bool => todo!("err"),
+                TyPrim::Void => todo!("err"),
+                TyPrim::Never => self.map.tys.get_or_intern(Ty::Prim(TyPrim::Never)),
+            },
+            Ty::Func { .. } => todo!("err"),
+            Ty::Slice(_) => todo!("err"),
+            Ty::Nullable(_) => todo!("err"),
+        }
+    }
+}
+
+impl TyPrim {
+    fn as_integer(&self) -> Option<(&bool, &Option<NonZeroU8>)> {
+        match self {
+            TyPrim::Integer { signed, size } => Some((signed, size)),
+            _ => None,
+        }
     }
 }
